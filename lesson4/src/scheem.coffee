1f4f9ca0c3bed12a53040a7ff2a1faa72371392c
William Bettridge-Radford <william.bettridge.radford@gmail.com>
2012-04-30 19:58:52 +0100

    mend

--- lesson4/src/scheem.coffee
+++ lesson4/src/scheem.coffee
@@ -1,29 +1,61 @@
-todo = """
-Numbers
-Variable references
-begin
-quote
-=
-<
-cons
-car
-cdr
-if
-"""
+parser = require('./parser').parser
+expect = require('chai').expect
+
+evalScheemString = (s, env) ->
+    return evalScheem parser(s), env
 
 evalScheem = (expr, env) ->
     # Numbers evaluate to themselves
     if typeof expr == 'number'
         return expr
+    if typeof expr == 'string'
+        return env[expr]
     # Look at head of list for operation
     switch expr[0]
+        when '='
+            q = evalScheem(expr[1], env) == evalScheem(expr[2], env)
+            return if q then '#t' else '#f'
         when '+'
             return evalScheem(expr[1], env) +
                    evalScheem(expr[2], env)
+        when '*'
+            return evalScheem(expr[1], env) *
+                   evalScheem(expr[2], env)
+        when '<'
+            q = evalScheem(expr[1], env) < evalScheem(expr[2], env)
+            return if q then '#t' else '#f'
+        when 'if'
+            q = evalScheem(expr[1], env)
+            if q == '#t'
+                return evalScheem expr[2], env
+            else
+                return evalScheem expr[3], env
+
+
         when 'quote'
             return expr[1]
+        when 'define'
+            env[expr[1]] = evalScheem expr[2], env
+            return 0
+        when 'begin'
+            expr.shift()
+            for e in expr
+                r = evalScheem e, env
+            return r
+        when 'car'
+            return evalScheem(expr[1], env)[0]
+        when 'cdr'
+            r = evalScheem(expr[1], env)
+            r.shift()
+            return r
+        when 'cons'
+            head = evalScheem expr[1], env
+            tail = evalScheem expr[2], env
+            tail.unshift head
+            return tail
 
-# If we are used as Node module, export evalScheem
 if typeof module != 'undefined'
     module.exports.evalScheem = evalScheem
+    module.exports.parser = parser
+    module.exports.evalScheemString = evalScheemString
 
--- lesson4/test/evalScheemTest.js
+++ lesson4/test/evalScheemTest.js
@@ -1,11 +1,23 @@
 if (typeof module !== 'undefined') {
     // In Node load required modules
     var assert = require('chai').assert;
-    var evalScheem = require('../js/scheem').evalScheem;
+    var scheem = require('../js/scheem');
+    var evalScheemString = scheem.evalScheemString;
+    var evalScheem = scheem.evalScheem;
+    var parser = scheem.parser;
 } else {
     // In browser assume already loaded by <script> tags
     var assert = chai.assert;
 }
+
+suite('number', function() {
+    test('3', function() {
+        assert.deepEqual(
+            evalScheem(3, {}),
+            3
+        );
+    });
+});
 
 suite('quote', function() {
     test('a number', function() {
@@ -24,6 +36,148 @@ suite('quote', function() {
         assert.deepEqual(
             evalScheem(['quote', [1, 2, 3]], {}),
             [1, 2, 3]
+        );
+    });
+});
+
+suite('variables', function() {
+    test('define a 3', function() {
+        var env = {};
+        assert.deepEqual(
+            evalScheem(["define", 'a', 3], env),
+            0
+        );
+        assert.deepEqual(
+            evalScheem('a', env),
+            3
+        );
+    });
+});
+
+suite('begin', function() {
+    test('(define a 3)', function() {
+        var env = {};
+        assert.deepEqual(
+            evalScheem(['begin', ["define", 'a', 3]], env),
+            0
+        );
+        assert.deepEqual(
+            evalScheem('a', env),
+            3
+        );
+    });
+    test('(define a 3) (+ a a)', function() {
+        var env = {};
+        assert.deepEqual(
+            evalScheem(['begin', ["define", 'a', 3], ['+', 'a', 'a']], env),
+            6
+        );
+    });
+});
+
+suite('equal', function() {
+    test('= 3 3', function() {
+        assert.deepEqual(
+            evalScheem(['=',3,3], {}),
+            '#t'
+        );
+    });
+    test('= 3 4', function() {
+        assert.deepEqual(
+            evalScheem(['=',3,4], {}),
+            '#f'
+        );
+    });
+});
+
+suite('less than', function() {
+    test('< 3 3', function() {
+        assert.deepEqual(
+            evalScheem(['<',3,3], {}),
+            '#f'
+        );
+    });
+    test('< 3 4', function() {
+        assert.deepEqual(
+            evalScheem(['<',3,4], {}),
+            '#t'
+        );
+    });
+});
+
+suite('if', function() {
+    test('(= 3 4) 9 10', function() {
+        assert.deepEqual(
+            evalScheem(['if', ['=',3,4], 9, 10], {}),
+            10
+        );
+    });
+    test('(= 3 3) 9 10', function() {
+        assert.deepEqual(
+            evalScheem(['if', ['=',3,3], 9, 10], {}),
+            9
+        );
+    });
+});
+
+suite('car', function() {
+    test('\'(1 2 3)', function() {
+        assert.deepEqual(
+            evalScheem(['car', ['quote', [1, 2, 3]]], {}),
+            1
+        );
+    });
+});
+
+suite('cdr', function() {
+    test('\'(1 2 3)', function() {
+        assert.deepEqual(
+            evalScheem(['cdr', ['quote', [1, 2, 3]]], {}),
+            [2, 3]
+        );
+    });
+});
+
+suite('cons', function() {
+    test('1 \'()', function() {
+        assert.deepEqual(
+            evalScheem(['cons', 1, ['quote', []]], {}),
+            [1]
+        );
+    });
+});
+
+suite('parser', function() {
+    var s1 = '(quote 1)';
+    var s2 = '(car (quote (1 2 3)))';
+    test(s1, function() {
+        assert.deepEqual(
+            parser(s1),
+            ['quote', 1]
+        );
+    });
+    test(s2, function() {
+        assert.deepEqual(
+            parser(s2),
+            ['car', ['quote', [1,2,3]]]
+        );
+    });
+});
+
+suite('evalScheemString', function() {
+    var s1 = '(car (quote (1 2 3)))';
+    test(s1, function() {
+        assert.deepEqual(
+            evalScheemString(s1),
+            1
+        );
+    });
+
+    var s2 = '(+ 2 (* 3 4))';
+    test(s2, function() {
+        assert.deepEqual(
+            evalScheemString(s2),
+            14
         );
     });
 });
